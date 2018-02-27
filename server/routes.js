8a0a5e7107037d4cd521f232ca7b8fff8a5fe42c
prathamesh-wagh <prathamesh.wagh@raweng.com>
2018-02-27 11:59:07 +0530

    1. Changes app.js and index.html.

--- server/routes.js
+++ server/routes.js
@@ -1,3 +1,24 @@
+const rest      = require('rest')
+const Built     = require('built.io')
+const when      = require('when')
+const API_KEY   = "bltd5a724a244ef9c9c"
+const masterKey = "blt313e0cb3131a7124"
+
+function getUserSession(builtApp, accessToken){
+  return builtApp
+  .setAccessToken(accessToken)
+  .User
+  .getSession(true);
+}
+
+function getMasterApp(builtApp){
+  return builtApp.setMasterKey(masterKey)
+}
+
+function getBuiltApp(req){
+  return req.builtApp.persistSessionWith(Built.Session.MEMORY);
+}
+
 module.exports = {
   "/v1/functions/test": {
     "GET": function(req, res){
