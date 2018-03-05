9725febaaf1fcc3e9eefacc4f33edcc53d381fda
prathamesh-wagh <prathamesh.wagh@raweng.com>
2018-03-05 18:26:04 +0530

    1. Changes app.js and index.html.

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 var Built = require('built-extension-sdk')
 
 // Initiate application
-var app = Built.App('bltd5a724a244ef9c9c')
-.setHost("dev-api.built.io")
+var app = Built.App('blt3f2adfc6b05abcf1')
+.setHost("stag-api.built.io")
 .setProtocol("https")
-.setMasterKey('blt313e0cb3131a7124')
+.setMasterKey('blt9291ac481447fcd0')
 
 var extensionSDK = app.Extension({
 	secret_key     : 'gochirp',
-	extension_key	 : 'blt_ext_default',
+	extension_key	 : 'blt_ext_default_chirp',
 	static         : __dirname + '/client',
 	routes         : require('./server/routes')
 })
